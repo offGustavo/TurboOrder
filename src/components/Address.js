baeced5738363832fc37bf18f9ade7c3fed16b80
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-03 12:05:59 -0300

    FIX: Adcionado container no address

--- src/components/Address.js
+++ src/components/Address.js
@@ -4,112 +4,114 @@ import "../styles/Address.css";
 
 const Address = ({ formData, handleChange }) => {
   return (
-    <Box sx={{ "& .MuiOutlinedInput-root": { width: "100%" } }}>
-      <div class="addressForm">
-        <TextField
-          label="CEP"
-          variant="outlined"
-          name="cli_cep"
-          value={formData.cli_cep || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "15ch",
-          }}
-        />
-        <TextField
-          label="Cidade"
-          variant="outlined"
-          name="cli_cidade"
-          value={formData.cli_cidade || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "30ch",
-          }}
-        />
-        <TextField
-          label="Bairro"
-          variant="outlined"
-          name="cli_bairro"
-          value={formData.cli_bairro || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "30ch",
-          }}
-        />
-        <TextField
-          label="Rua"
-          variant="outlined"
-          name="cli_rua"
-          value={formData.cli_rua || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "40ch",
-          }}
-        />
-        <TextField
-          label="Número"
-          variant="outlined"
-          name="cli_numero"
-          value={formData.cli_numero || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "10ch",
-          }}
-        />
-        <TextField
-          label="Complemento"
-          variant="outlined"
-          name="cli_complemento"
-          value={formData.cli_complemento || ""}
-          onChange={handleChange}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": { borderColor: "#FD1F4A" },
-              "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
-            },
-            "& .MuiInputBase-input": {
-              color: "black",
-            },
-            width: "25ch",
-          }}
-        />
-      </div>
-    </Box>
+    <div class="addressContainer">
+      <Box sx={{ "& .MuiOutlinedInput-root": { width: "100%" } }}>
+        <div class="addressForm">
+          <TextField
+            label="CEP"
+            variant="outlined"
+            name="cli_cep"
+            value={formData.cli_cep || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "15ch",
+            }}
+          />
+          <TextField
+            label="Cidade"
+            variant="outlined"
+            name="cli_cidade"
+            value={formData.cli_cidade || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "30ch",
+            }}
+          />
+          <TextField
+            label="Bairro"
+            variant="outlined"
+            name="cli_bairro"
+            value={formData.cli_bairro || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "30ch",
+            }}
+          />
+          <TextField
+            label="Rua"
+            variant="outlined"
+            name="cli_rua"
+            value={formData.cli_rua || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "40ch",
+            }}
+          />
+          <TextField
+            label="Número"
+            variant="outlined"
+            name="cli_numero"
+            value={formData.cli_numero || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "10ch",
+            }}
+          />
+          <TextField
+            label="Complemento"
+            variant="outlined"
+            name="cli_complemento"
+            value={formData.cli_complemento || ""}
+            onChange={handleChange}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": { borderColor: "#FD1F4A" },
+                "&.Mui-focused fieldset": { borderColor: "#FD1F4A" },
+              },
+              "& .MuiInputBase-input": {
+                color: "black",
+              },
+              width: "25ch",
+            }}
+          />
+        </div>
+      </Box>
+    </div>
   );
 };
 
