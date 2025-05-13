b0f088a9896b2529c72400b61ef63c8fd72851ba
AnnielyBorges <neykhenney@gmail.com>
2025-05-13 20:29:58 -0300

    CH: modificad a estilização do componente OrderCard

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -3,14 +3,22 @@ import { FaPen } from "react-icons/fa";
 import '../styles/OrderCard.css';
 
 const OrderCard = ({ id, name, details, status, data }) => {
-  const statusClass = status.toLowerCase().replace(' ', '-');
+  const statusMap = {
+    0: "Em andamento",
+    1: "Concluído",
+    2: "Cancelado"
+  };
+  
+  const statusText = typeof status === 'string' ? status : statusMap[status] || "Desconhecido";
+  const statusClass = statusText.toLowerCase().replace(' ', '-');
+
   
   return (
     <div className="order-card">
       <div className="order-header">
         <div>
           <span className={`order-id`}>Id: {id}</span>
-          <span className={`status-tag ${statusClass}`}>{status}</span>
+          <span className={`status-tag ${statusClass}`}>{statusText}</span> {/* Aqui, use statusText */}
         </div>
         <span className="order-date">{data}</span>
       </div>
