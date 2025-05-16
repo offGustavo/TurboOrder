e2d5a5cc0ad5d22104cc1153cb0aaab266a5734f
Luiz Gustavo <luizgmdcm@gmail.com>
2025-05-16 00:34:41 -0300

    ADD: adicionado elemento para mostrar a ordem do pedido no dia

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { FaPen } from "react-icons/fa";
 import '../styles/OrderCard.css';
 
-const OrderCard = ({ id, name, details, status, data }) => {
+const OrderCard = ({ id, name, details, status, data, day_order }) => {
   const statusMap = {
     0: "Em andamento",
     1: "Concluído",
     2: "Cancelado"
   };
-  
+
+  // let day_order = 11;
+
   const statusText = typeof status === 'string' ? status : statusMap[status] || "Desconhecido";
   const statusClass = statusText.toLowerCase().replace(' ', '-');
 
-  
+
   return (
     <div className="order-card">
       <div className="order-header">
@@ -20,7 +22,11 @@ const OrderCard = ({ id, name, details, status, data }) => {
           <span className={`order-id`}>Id: {id}</span>
           <span className={`status-tag ${statusClass}`}>{statusText}</span> {/* Aqui, use statusText */}
         </div>
-        <span className="order-date">{data}</span>
+        <div className='order-date-day'>
+          <span className="order-date">{data}</span>
+          {/* TODO: Criar o indicador de ordem para o dia */}
+          <div className='order-day-order'><span>{day_order}</span></div>
+        </div>
       </div>
       <p className="customer-name">{name}</p>
       <p className="order-details">{details}</p>
